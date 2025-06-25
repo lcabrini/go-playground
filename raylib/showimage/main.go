9c9eebe461e4f2da88763e9089e3f98143f58904
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-25 22:17:47 +0000

    Scale image

--- raylib/showimage/main.go
+++ raylib/showimage/main.go
@@ -11,6 +11,9 @@ const (
 	Width  int32  = 1024
 	Height int32  = 768
 	Title  string = "Image"
+
+	MidX int32 = Width / 2
+	MidY int32 = Height / 2
 )
 
 func main() {
@@ -25,12 +28,28 @@ func main() {
 
 	texture := rl.LoadTexture(os.Args[1])
 
+	var scale float32 = 1.0
+	tw := float32(texture.Width)
+	th := float32(texture.Height)
+	if texture.Height > Height || texture.Width > Width {
+		scaleX := float32(Width) / tw
+		scaleY := float32(Height) / th
+		scale = scaleX
+		if scaleY < scaleX {
+			scale = scaleY
+		}
+
+		tw *= scale
+		th *= scale
+	}
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
-		rl.DrawTexture(texture, 0, 0, rl.White)
+		pos := rl.Vector2{X: float32(MidX) - tw/2.0, Y: float32(MidY) - th/2.0}
+		rl.DrawTextureEx(texture, pos, 0, scale, rl.White)
 		rl.EndDrawing()
 	}
 
+	rl.UnloadTexture(texture)
 	rl.CloseWindow()
 }
