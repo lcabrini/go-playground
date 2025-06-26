fe3cb062b879f270ffc79f9a3a9d5516ea861278
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2025-06-26 22:41:35 +0000

    Print basename of the executable

--- args/main.go
+++ args/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	fmt.Printf("Name of the executable: %s\n", os.Args[0])
+	name := filepath.Base(os.Args[0])
+	fmt.Printf("Name of the executable: %s\n", name)
 
 	for i, a := range os.Args[1:] {
 		fmt.Printf("%2d. %s\n", i, a)
